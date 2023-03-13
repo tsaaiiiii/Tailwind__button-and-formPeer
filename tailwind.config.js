2329cfcb1450cbf5584716e0ee297003fae931e9
ting <a0909513991@gmail.com>
2023-03-13 11:09:55 +0800

    完成第一版

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        palete: {
+          red: "#d1473f",
+          blue: "#1b2e43",
+          yellow: "#d6984d",
+          green: "#7a9989",
+          orange: "#dd561f",
+          pink: "#eaafa1",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
